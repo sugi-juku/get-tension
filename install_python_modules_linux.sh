013b0dcd3cfea2d8ba974ad5d68221adc3807b85
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2020-06-19 00:42:01 +0900

    Update

--- install_python_modules_linux.sh
+++ install_python_modules_linux.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -x
 
+sudo apt install portaudio19-dev
 pip install numpy
 pip install matplotlib
 pip install PyAudio
 pip install scikit-learn
+pip3 install numpy
+pip3 install matplotlib
+pip3 install PyAudio
+pip3 install scikit-learn
