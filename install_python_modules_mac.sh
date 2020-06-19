a1c12c93004578ea0c22ed7c2738a4d41a3a2bb5
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2020-06-19 15:57:08 +0900

    Updated

--- install_python_modules_mac.sh
+++ install_python_modules_mac.sh
@@ -5,3 +5,9 @@ pip install numpy
 pip install matplotlib
 pip install PyAudio
 pip install scikit-learn
+pip install pathlib
+pip3 install numpy
+pip3 install matplotlib
+pip3 install PyAudio
+pip3 install scikit-learn
+pip3 install pathlib
