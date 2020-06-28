8515257e04a9c7f7aa65f2d39c203050db2809b3
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2020-06-28 16:13:22 +0900

    Initial import

--- install_python_modules_win.bat
+++ install_python_modules_win.bat
@@ -1,5 +1,8 @@
+@echo off
+py -m pip install --upgrade pip
 py -m pip install numpy
 py -m pip install matplotlib
-py -m pip install PyAudio
+py -m pip install PyAudio‑0.2.11‑cp38‑cp38‑win_amd64.whl
 py -m pip install scikit-learn
 py -m pip install pathlib
+set /P str="Hit any KEY to exit."
