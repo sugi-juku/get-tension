115f9f24efda6d3fb3be68afb2dd4ce5bfbeb9b0
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2020-07-02 19:14:30 +0900

    Added portaudio

--- install_python_modules_linux.sh
+++ install_python_modules_linux.sh
@@ -7,7 +7,7 @@ sudo apt install python3-pip
 sudo apt install python3-dev
 sudo apt install python3-tk
 sudo apt install tk-dev
-# sudo apt install portaudio19-dev
+sudo apt install portaudio19-dev
 # pip install numpy
 # pip install matplotlib==2.0.2
 # pip install PyAudio
