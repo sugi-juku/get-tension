07295390895c4202cc61bac3b90b843402bfd5e9
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2022-01-31 17:36:23 +0900

    Initial import

---
+++ upgrade_python_modules.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+pip3 install --upgrade pip
+pip3 install --upgrade numpy
+pip3 install --upgrade matplotlib
+pip3 install --upgrade sounddevice
+pip3 install --upgrade soundfile
+pip3 install --upgrade scikit-learn
+pip3 install --upgrade pathlib
---
+++ upgrade_python_modules_win.bat
@@ -0,0 +1,9 @@
+@echo off
+py -m pip install --upgrade pip
+py -m pip install --upgrade numpy
+py -m pip install --upgrade matplotlib
+py -m pip install --upgrade sounddevice
+py -m pip install --upgrade soundfile
+py -m pip install --upgrade scikit-learn
+py -m pip install --upgrade pathlib
+set /P str="Hit any KEY to exit."
