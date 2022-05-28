edcbd22945d9a2ccde57017b2557dd38825bb5d6
Yukihiko Sugimura <sugimura_juku@yahoo.co.jp>
2022-05-28 14:35:37 +0900

    Modified about f0 search method

--- string_fft.py
+++ string_fft.py
@@ -6,6 +6,7 @@ import sys
 import os
 import soundfile as sfile
 import matplotlib.pyplot as plt
+import scipy.signal as sig
 
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
@@ -28,18 +29,18 @@ class StringFft:
         # Frequency Search range
         #
         add_freq = {}
-        add_freq["B"] = 130
-        add_freq["T"] = 115
+        add_freq["B"] = 170
+        add_freq["T"] = 170
         x_smin = {}
-        x_smin["B"] = 950
-        x_smin["T"] = 450
+        x_smin["B"] = 930
+        x_smin["T"] = 460
         # Polyfit coef
         pf_a = {}
-        pf_a["B"] = 0.0272
-        pf_a["T"] = 0.06
+        pf_a["B"] = 0.027
+        pf_a["T"] = 0.056
         pf_b = {}
-        pf_b["B"] = -10.363
-        pf_b["T"] = 11.884
+        pf_b["B"] = -10.1
+        pf_b["T"] = 14.1
         x_smax = {}
         x_smax[stype] = (tension-pf_b[stype])/pf_a[stype]+add_freq[stype]
  
@@ -55,22 +56,31 @@ class StringFft:
         fft_data = np.abs(np.fft.fft(data))
         freq_data = np.fft.fftfreq(data.shape[0], d=1.0/fs)
 
-        if plt_show == 1:
-            plt.plot(freq_data, fft_data)
-            plt.xlim(0, 3000)
-            plt.show()
+        peak_i = sig.argrelmax(fft_data, order=256)[0]
 
         self.max_y=0.0
         self.max_x=0.0
         self.max_i=0
 
         # Search f0
-        for i,val in enumerate(freq_data):
+        # peak_cnt = 0
+        for i in peak_i:
             if x_smin[stype] <= freq_data[i] <= x_smax[stype]:
                 if self.max_y < fft_data[i]:
+                    # peak_cnt += 1
+                    # Get first peak
+                    # if peak_cnt < 2:
                     self.max_y=fft_data[i]
                     self.max_x=freq_data[i]
                     self.max_i=i
+
+        if plt_show == 1:
+            plt.plot(freq_data, fft_data)
+            plt.xlim(0, 3000)
+            plt.vlines(x_smin[stype], 0, self.max_y, 'r', linestyles='dashed')
+            plt.vlines(x_smax[stype], 0, self.max_y, 'r', linestyles='dashed')
+            plt.plot(freq_data[peak_i], fft_data[peak_i], 'ro')
+            plt.show()
 
         # print(max(fft_data[0:self.max_i+50]))
         # print(np.argmax(fft_data[0:self.max_i+50]))
@@ -84,7 +94,7 @@ class StringFft:
         return self.max_x
 
 if __name__ == "__main__":
-    sf = StringFft("wavdata/20200222163539_T_45_GOAKP16-YOPTF120_98_16-19.wav")
+    sf = StringFft("wavdata/20210811200208_T_40_TFXONE124_95_16-20.wav", plt_show=1)
     print(sf.get_f0())
-    sf = StringFft("wavdata/20201017154943_B_22_YOBGAS_56.wav", plt_show=1)
+    sf = StringFft("wavdata/20200923192223_B_24_YOBG66UM_56.wav", plt_show=1)
     print(sf.get_f0())
